9d258d9e6ad1fb613e1e4b5ff001825012261e48
jeffIrvine <jirvine1212@gmail.com>
2021-05-14 18:15:15 -0700

    pulling images

--- bSoup.py
+++ bSoup.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib.request
 
 url = "https://secretlab.co/collections"
 
@@ -7,4 +8,14 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-print(soup.prettify())
+# print(soup.prettify())
+
+images = soup.find_all("img")
+
+number = 0
+
+for image in images:
+  image_src = image["src"]
+  print(image_src)
+  urllib.request.urlretrieve(image_src)
+  number += 1
