a034b513eee5bcfc697343b02cc3f5c5469d091d
jeffIrvine <jirvine1212@gmail.com>
2021-05-30 22:20:03 -0700

    that is sooo cool website navigation and small automation

--- seleniumTest/seleniumTest.py
+++ seleniumTest/seleniumTest.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-
 driver = webdriver.Chrome(PATH)
 
-driver.get('https://google.com')
+driver.maximize_window()
+driver.get('https://store.hermanmiller.com/')
+
+python_button = driver.find_element_by_xpath("/html/body/div[1]/header/nav/div/div/div[2]/div[2]/a")
+python_button.click()
+search = driver.find_element_by_name("q")
+search.send_keys("Aeron")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
